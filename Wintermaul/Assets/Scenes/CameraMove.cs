43ed49e47ca9d4e0101e57ac92ae89685a528e43
Martí Serra Plomer <rodesdecarro@gmail.com>
2019-03-21 20:12:26 +0100

    Add background image and side menu. Add limits to camera movement. Add X axis camera movement.

--- Wintermaul/Assets/Scenes/CameraMove.cs
+++ Wintermaul/Assets/Scenes/CameraMove.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    private readonly float MoveSpeed = 1f;
+    private readonly float MoveSpeed = 0.5f;
+    private readonly float DragSpeed = 0.05f;
     private readonly float MinZoom = 3f;
     private readonly float MaxZoom = 9f;
     private readonly float MinY = -39f;
     private readonly float MaxY = 1f;
+    private readonly float MinX = 0f;
+    private readonly float MaxX = 3f;
     private float MouseStartY;
     private float MouseMoveY;
+    private float MouseStartX;
+    private float MouseMoveX;
 
     /// <summary>
     /// Start is called before the first frame update
@@ -25,24 +28,62 @@ public class CameraMove : MonoBehaviour
     /// </summary>
     void Update()
     {
-        CheckArrowMovement();
+        CheckMovement();
         CheckMouseWheelZoom();
-        CheckDrag();
     }
 
-    /// <summary>
-    /// Moves the camera if any arrow is pressed
-    /// </summary>
-    private void CheckArrowMovement()
+    private void CheckMovement()
     {
+        float newY = transform.position.y;
+        float newX = transform.position.x;
+
+        // Check arrows pressed
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - MoveSpeed, transform.position.z);
+            newY -= MoveSpeed;
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + MoveSpeed, transform.position.z);
+            newY += MoveSpeed;
+        }
+
+        // Check arrows pressed
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            newX -= MoveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            newX += MoveSpeed;
+        }
+
+        // Check drag
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseStartY = Input.mousePosition.y;
+            MouseStartX = Input.mousePosition.x;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            MouseMoveY = Input.mousePosition.y - MouseStartY;
+            MouseMoveX = Input.mousePosition.x - MouseStartX;
+
+            MouseStartY = Input.mousePosition.y;
+            MouseStartX = Input.mousePosition.x;
+
+            newY -= MouseMoveY * DragSpeed;
+            newX -= MouseMoveX * DragSpeed;
+        }
+
+        newY = Mathf.Clamp(newY, MinY, MaxY);
+        newX = Mathf.Clamp(newX, MinX, MaxX);
+
+        // Update position
+        if (newX != transform.position.x || newY != transform.position.y)
+        {
+            transform.position = new Vector3(newX, newY, transform.position.z);
         }
     }
 
@@ -68,24 +109,6 @@ public class CameraMove : MonoBehaviour
             }
         }
     }
-
-    /// <summary>
-    /// Check if the player is dragging the map to move the camera
-    /// </summary>
-    private void CheckDrag()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            MouseStartY = Input.mousePosition.y;
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            MouseMoveY = Input.mousePosition.y - MouseStartY;
-            MouseStartY = Input.mousePosition.y;
-            transform.position = new Vector3(transform.position.x, transform.position.y + MouseMoveY * transform.position.z * 0.005f, transform.position.z);
-        }
-    }
-
 }
 
 
