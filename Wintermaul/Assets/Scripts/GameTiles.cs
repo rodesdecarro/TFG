dc7a8d2c9330660e82dadde645ce7d7f0c30c30d
Martí Serra Plomer <rodesdecarro@gmail.com>
2019-04-06 10:40:38 +0200

    Add Gems asset and tile handler

---
+++ Wintermaul/Assets/Scripts/GameTiles.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameTiles : MonoBehaviour
+{
+    public static GameTiles instance;
+    public Tilemap Tilemap;
+
+    public Dictionary<Vector3, WorldTile> tiles;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        GetWorldTiles();
+    }
+
+    // Use this for initialization
+    private void GetWorldTiles()
+    {
+        tiles = new Dictionary<Vector3, WorldTile>();
+
+        foreach (Vector3Int pos in Tilemap.cellBounds.allPositionsWithin)
+        {
+            // Exclude map borders
+            if (pos.x < -4 || pos.x > 4 || pos.y > 2 || pos.y < -40) continue;
+
+            var localPlace = new Vector3Int(pos.x, pos.y, pos.z);
+
+            var tile = new WorldTile
+            {
+                LocalPlace = localPlace,
+                WorldLocation = Tilemap.CellToWorld(localPlace),
+                TileBase = Tilemap.GetTile(localPlace),
+                TilemapMember = Tilemap,
+                Name = localPlace.x + "," + localPlace.y,
+                Buildable = true,
+                Cost = 1
+            };
+
+            // Startregion
+            if (pos.x > -3 && pos.x < 3 && pos.y > -3 && pos.y < 3)
+            {
+                tile.Buildable = false;
+            }
+
+            // Endregion
+            if (pos.x > -3 && pos.x < 3 && pos.y > -41 && pos.y < -35)
+            {
+                tile.Buildable = false;
+            }
+
+            tiles.Add(tile.WorldLocation, tile);
+        }
+    }
+}
