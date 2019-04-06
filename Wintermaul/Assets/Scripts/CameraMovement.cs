f113881073c7599b1bed4de88f63ba3eb426d33c
Martí Serra Plomer <rodesdecarro@gmail.com>
2019-04-06 16:40:42 +0200

    Refactorized map creation, and tile handler

---
+++ Wintermaul/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float cameraSpeed;
+
+    private float xMax;
+    private float yMin;
+
+    private float MouseStartY;
+    private float MouseMoveY;
+    private float MouseStartX;
+    private float MouseMoveX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseStartY = Input.mousePosition.y;
+            MouseStartX = Input.mousePosition.x;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            MouseMoveY = Input.mousePosition.y - MouseStartY;
+            MouseMoveX = Input.mousePosition.x - MouseStartX;
+
+            MouseStartY = Input.mousePosition.y;
+            MouseStartX = Input.mousePosition.x;
+
+
+            transform.Translate(Vector3.down * MouseMoveY * Time.deltaTime);
+            transform.Translate(Vector3.left * MouseMoveX * Time.deltaTime);
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 0, xMax), Mathf.Clamp(transform.position.y, yMin, 0), transform.position.z);
+    }
+
+    public void SetLimits(Vector3 maxTile)
+    {
+        Vector3 worldPoint = Camera.main.ViewportToWorldPoint(new Vector3(1, 0));
+
+        xMax = Mathf.Max(0, maxTile.x - worldPoint.x);
+        yMin = Mathf.Min(0, maxTile.y - worldPoint.y);
+    }
+}
---
+++ Wintermaul/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    [SerializeField]
+    private GameObject[] tilePrefabs;
+
+    [SerializeField]
+    private CameraMovement cameraMovement;
+
+    [SerializeField]
+    private Transform map;
+
+    private Point blueSpawn;
+    private Point redSpawn;
+
+    public Dictionary<Point, TileScript> Tiles { get; set; }
+
+    public float TileSize
+    {
+        get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void CreateLevel()
+    {
+        Tiles = new Dictionary<Point, TileScript>();
+
+        string[] mapData = ReadLevelText();
+
+        int mapX = mapData[0].Length;
+        int mapY = mapData.Length;
+
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+
+        for (int y = 0; y < mapY; y++)
+        {
+            for (int x = 0; x < mapX; x++)
+            {
+                PlaceTile((int)char.GetNumericValue(mapData[y][x]), x, y, worldStart);
+            }
+        }
+
+        Vector3 maxTile = Tiles[new Point(mapX - 1, mapY - 1)].transform.position;
+
+        cameraMovement.SetLimits(new Vector3(maxTile.x + TileSize, maxTile.y - TileSize));
+    }
+
+    private string[] ReadLevelText()
+    {
+        TextAsset data = (TextAsset)Resources.Load("Level");
+
+        return data.text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+    }
+
+    private void PlaceTile(int tileType, int x, int y, Vector3 worldStart)
+    {
+        if (tileType == 4)
+        {
+            blueSpawn = new Point(x, y);
+        }
+
+        if (tileType == 2)
+        {
+            redSpawn = new Point(x, y);
+        }
+
+        TileScript newTile = Instantiate(tilePrefabs[tileType]).GetComponent<TileScript>();
+
+        newTile.Setup(new Point(x, y), new Vector3(worldStart.x + x * TileSize, worldStart.y - y * TileSize, 0), map);
+    }
+}
